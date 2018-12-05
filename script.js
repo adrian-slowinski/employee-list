1077fc65ea6fdb10917839f6d0a0209ff7f5dcea
Adrian Słowiński <adrian.slowinski87@gmail.com>
2018-12-05 11:15:23 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function createEmployee(firstName, lastName, age, sex, employed){
+  return{
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    sex: sex,
+    employed: employed
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
