176c7af47a80201aaa0f8012c9f33ac221a532ac
Adrian Słowiński <adrian.slowinski87@gmail.com>
2018-12-08 16:46:57 +0100

    Add index column

--- script.js
+++ script.js
@@ -11,45 +11,59 @@ let employeers = [];
 employeers.push(createEmployee("John", "Doe", "31", "male", true));
 employeers.push(createEmployee("Amelia", "Clark", "21", "female", true));
 employeers.push(createEmployee("Jack", "Jones", "31", "male", true));
-employeers.push(createEmployee("Olivia", "Wilde", "26", "male", true));
+employeers.push(createEmployee("Olivia", "Wilde", "26", "male", true)); 
 
 
 function fillEmployeeTable(employeers) {
-    let employersDOM = document.querySelector(".employeers");
-    let tabHead = employersDOM.querySelector(".table__head").querySelector(".table__row");
+	let employersDOM = document.querySelector(".employeers");
+	let tabHead = employersDOM.querySelector(".table__head").querySelector(".table__row");
 
-    function showKeyName() {
-        let employersHeaders = Object.keys(employeers[0]); // Pobranie nazw kluczy obiektu 
-        let employersHeadersLength = employersHeaders.length; // Pobranie długości klucza
+	function showKeyName() {
+		let employersHeaders = Object.keys(employeers[0]); // Pobranie nazw kluczy obiektu 
+		let employersHeadersLength = employersHeaders.length; // Pobranie długości klucza
 
-        let th = "";
-        for (let i = 0; i < employersHeadersLength; i++) { // Przeiterowanie każdego elementu 
-            th = document.createElement('th');
-            th.setAttribute("scope", "col");
-            th.classList.add("table__head--item");
-            th.textContent = employersHeaders[i].toLocaleUpperCase();
+		let th = "";
+		for (let i = 0; i < employersHeadersLength; i++) { // Przeiterowanie każdego elementu 
+			th = document.createElement('th');
+			th.setAttribute("scope", "col");
+			th.classList.add("table__head--item");
+			th.textContent = employersHeaders[i].toLocaleUpperCase();
 
-            tabHead.appendChild(th);
-        }
-    }
+			tabHead.appendChild(th);
+		}  
+			function addIndex() {
+				let th = "";
+				th = document.createElement('th');
+				th.setAttribute("scope", "col");
+				th.classList.add("table__head--item");
+				th.textContent = "Index".toLocaleUpperCase();
 
-    function fillCels(employeers) {
-        let tabBody = employersDOM.querySelector(".table__body");
+				tabHead.prepend(th);
+			}
+			addIndex()
+	} 
 
-        employeers.forEach(function(worker) {
-            tabBody.innerHTML += `<tr class="table__row"><td> ${worker.firstName} </td>  
-        <td> ${worker.lastName} </td>  
-        <td> ${worker.age} </td>  
-        <td> ${worker.sex} </td>  
-        <td> ${worker.employed} </td> </tr>
-        `;
+	function fillCels(employeers) {
+		let tabBody = employersDOM.querySelector(".table__body");
 
-        });
+		employeers.forEach(function(worker, index) {
 
-    }
-
-    showKeyName();
-    fillCels(employeers);
+		tabBody.innerHTML += `<tr class="table__row">
+			<td> ${index += 1} </td>
+			<td> ${worker.firstName}</td>   
+			<td> ${worker.lastName} </td>   
+			<td> ${worker.age} </td>  
+			<td> ${worker.sex} </td>  
+			<td> ${worker.employed} </td> </tr>
+		`; 
+		}); 
+	} 
+	showKeyName();
+	fillCels(employeers);
 }
 
 fillEmployeeTable(employeers);
+
+
+
+console.log(employeers);
